d587bbe03e241b58e533ae8c58c8650d040609f8
Richard Harrah <topplethenunnery@gmail.com>
2015-08-14 10:54:54 -0400

    fixing imports for Validate

--- src/main/java/com/tealcube/minecraft/bukkit/TextUtils.java
+++ src/main/java/com/tealcube/minecraft/bukkit/TextUtils.java
@@ -22,9 +22,9 @@
  */
 package com.tealcube.minecraft.bukkit;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.Validate;
-import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.bukkit.ChatColor;
 
 import java.util.ArrayList;
